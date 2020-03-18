78d01b3c07acd5bdcf2f592c4a921cc6f500d920
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-03-18 22:29:10 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ var color = document.getElementById("color");
 var colorlabel = document.getElementById("colors-js-puns");
 var design = document.getElementById("design");
 console.log(other);
+var activity = document.querySelector(".activities");
+var activityinput = document.querySelectorAll(".activities input");
 
 
 
@@ -22,51 +24,147 @@ function hideSelectTheme()
 
 
 design.addEventListener("change", (event) => {
-       //1Select, nothing changes, 
-       //2change, it goes to the else statement regardless
-        //3 click, it goes to the else statement regardless
+    //1Select, nothing changes, 
+    //2change, it goes to the else statement regardless
+    //3 click, it goes to the else statement regardless
 
-    if (event.target.value === "select theme") { // returns error select theme is undefined
-         //1 (event.SelectTheme) works
+    if (event.target.value === "select theme") {
 
-        function hideColor() {
+
+   //     for (i = 0; i < colorlabel.length; i++) {
+
+
 
             color.style.display = "none";
-            var newselectID = document.createElement("select");
-            var option = document.createElement("option");
-            option.textContent = "Please choose theme";
-            newselectID.appendChild(option);
-            colorlabel.appendChild(newselectID);
 
-            console.log("here");
 
         }
 
-        hideColor()
 
+
+
+
+  //  }
+
+    else if (event.target.value === "js puns") {
+
+
+
+        for (i = 0; i < color.length; i++) {
+
+            color.style.display = "block";
+            color[i].style.display = "none";
+            var cornflowerblue = document.querySelector('option[value="cornflowerblue"]');
+            var darkslategrey = document.querySelector('option[value="darkslategrey"]');
+            var gold = document.querySelector('option[value="gold"]');
+
+            cornflowerblue.style.display = "block";
+            darkslategrey.style.display = "block";
+            gold.style.display = "block";
+
+
+
+
+
+
+
+
+        }
+    
+    }
+
+
+
+
+
+    else if (event.target.value === "heart js") {
+
+
+
+        for (i = 0; i < color.length; i++) {
+
+            color.style.display = "block";
+            color[i].style.display = "none";
+            var tomato = document.querySelector('option[value="tomato"]');
+            var steelblue = document.querySelector('option[value="steelblue"]');
+            var dimgrey = document.querySelector('option[value="dimgrey"]');
+
+            tomato.style.display = "block";
+            steelblue.style.display = "block";
+            dimgrey.style.display = "block";
+
+
+
+
+
+
+
+
+        }
+    }
+}
+);
+
+var activityLabel = document.createElement("label");
+activity.appendChild(activityLabel);
+
+
+var totalactivityCost = 0;
+
+activity.addEventListener("change", (e) => {
+
+    var activityclicked = event.target;
+   
+    var activitycost = activityclicked.getAttribute("data-cost");
+   var activitycostint = parseInt(activitycost);
+
+
+
+    if (activityclicked.checked) {
+
+
+        totalactivityCost += activitycostint;
+        console.log(totalactivityCost);
+        activityLabel.textContent = "Total: " + totalactivityCost + " $"
+        var datadayandtime = activityclicked.getAttribute(`data-day-and-time`);
+        //console.log(datadayandtime + "date and time");
+    
 
     }
 
-    else {
+    for (i = 0; i < activityinput.length; i++) {
 
-        console.log("in here");
-        color.style.display = "block";
+      
+        var datadayandtimeI = activityinput[i].getAttribute(`data-day-and-time`);
+       
+        if (datadayandtime === datadayandtimeI && activityclicked !== activityinput[i]) {
+
+            console.log(activityinput[i]);
+
+            if (activityclicked.checked) {
+
+                activityinput[i].disabled = true;
+
+            }
+
+            else {
+                activityinput[i].disabled = false;
+                console.log("test");
+
+            }
+
+          
 
 
+            }
+        
+
+
+        }
 
     }
-});
 
 
 
-
-
-
-
-hideSelectTheme()
-
-hideOther()
-//hideColor()
-
-
+);
 
